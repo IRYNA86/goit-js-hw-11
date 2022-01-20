c63a0078798226fd475abdbc442dda9880d04f7b
Iryna Hudymenko <iryn4ik09@gmail.com>
2022-01-21 00:23:35 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import articleOnePhoto from './templates/templatesOnePhoto.hbs';
+const axios = require('axios').default;
+
